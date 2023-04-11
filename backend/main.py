895e1ba0d7dc22d87002a558594c30b88e6c6258
TaylanAkyurek <79318145+TaylanAkyurek@users.noreply.github.com>
2023-04-11 17:02:37 +0300

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,194 @@
-from fastapi import FastAPI, Form
+
 from fastapi.responses import HTMLResponse
 import sqlite3
+import numpy as np
+from matplotlib import pyplot as plt
 
-from starlette.responses import RedirectResponse
+from datetime import datetime, timedelta
+from typing import Annotated, Union
+
+from fastapi import Depends, FastAPI, HTTPException, status, Form, Request
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+
+from fastapi.templating import Jinja2Templates
+import json
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "143169a6c0fe9c5c55cb347539682a52f3afecf6e1e780135bc475f3c8c9bcd0"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "taylan": {
+        "username": "taylan",
+        "full_name": "taylan akyurek",
+        "email": "aakyurek17@gmail.comm",
+        "hashed_password": "$2b$12$qUcml2YPMHxNsZ.ANcTq5uWVjkqQ/oYpwroXC3hTCWzizoKYY.146",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: Union[str, None] = None
+    password: Union[str, None] = None
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(conn, username: str, password: str):
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM users WHERE username='{username}';")
+    user = cur.fetchone()
+    return user
+
+def authenticate_user(username: str, password: str):
+
+    conn = create_connection()
+    create_user_table(conn)
+    user = get_user(conn, username, password)
+    if not user:
+        print(f"User {username} does not exist")
+
+        return False
+
+    if not verify_password(password, user[2]):
+        print(f"Wrong password")
+        return False
+
+    print(f"User {username} authenticated successfully")
+    userObj = User()
+    userObj.username = user[1]
+    userObj.password = user[2]
+
+    return userObj
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+
+
+
+@app.post("/signup")
+def signup(username: str = Form(...), password: str = Form(...)):
+    conn = create_connection()
+    create_user_table(conn)
+    # Check if the username already exists
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM users WHERE username='{username}';")
+    user = cur.fetchone()
+
+    if user:
+
+        return {"fail message": "user already exists"}
+    else:
+        insert_user(conn, username, get_password_hash(password))
+        return {"success message": get_password_hash(password)}
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    conn = create_connection()
+    create_user_table(conn)
+    userFroDb = get_user(conn,username=token_data.username)
+    user = User()
+    user.username = userFroDb[1]
+    user.password = userFroDb[2]
+
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+
+async def get_current_active_user(
+        current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 # Define a function to create database connection
 def create_connection():
@@ -30,6 +213,30 @@ def create_user_table(conn):
         print(e)
 
 
+# Define a function to create database connection
+def create_data_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('data.db')
+        print("Connected to SQLite database")
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+
+# Define a function to create a wound data table
+def create_data_table(conn):
+    try:
+        conn.execute('''CREATE TABLE IF NOT EXISTS data
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                patient_id TEXT NOT NULL,
+                patient_name TEXT NOT NULL)
+                doctor TEXT NOT NULL);''')
+        print("woundcare data table created successfully")
+        insert_patient(conn, "1234", "taylan", "asli")
+    except sqlite3.Error as e:
+        print(e)
+
 # Define a function to insert a user into the table
 def insert_user(conn, username, password):
     try:
@@ -39,22 +246,15 @@ def insert_user(conn, username, password):
     except sqlite3.Error as e:
         print(e)
 
-
-# Define a function to authenticate user
-def authenticate_user(conn, username, password):
+def insert_patient(conn, patient_id, patient_name, doctor):
     try:
-        cur = conn.cursor()
-        cur.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}';")
-        user = cur.fetchone()
-        if user:
-            print(f"User {username} authenticated successfully")
-            return True
-        else:
-            print(f"User {username} authentication failed")
-            return False
+        conn.execute(f"INSERT INTO data (patient_id, patient_name, doctor) VALUES ('{patient_id}', '{patient_name}', '{doctor}');")
+        conn.commit()
+        print(f"Patient {patient_id} inserted successfully")
     except sqlite3.Error as e:
         print(e)
-        return False
+
+# Define a function to authenticate user
 
 
 # Define a route for the login page
@@ -75,27 +275,27 @@ def main_page():
                     margin: 50px auto;
                     width: 400px;
                 }
-    
+
                 /* Styling for the h1 tags */
                 h1 {
                     font-size: 28px;
                     text-align: center;
                 }
-    
+
                 /* Styling for the hr tag */
                 hr {
                     border: 0;
                     border-top: 1px solid #ccc;
                     margin: 20px 0;
                 }
-    
+
                 /* Styling for the label tags */
                 label {
                     font-size: 18px;
                     display: block;
                     margin-bottom: 10px;
                 }
-    
+
                 /* Styling for the input tags */
                 input[type=text], input[type=password] {
                     width: 100%;
@@ -105,7 +305,7 @@ def main_page():
                     border: 2px solid #ccc;
                     border-radius: 4px;
                 }
-    
+
                 /* Styling for the submit buttons */
                 button[type=submit] {
                     background-color: #4CAF50;
@@ -117,7 +317,7 @@ def main_page():
                     cursor: pointer;
                     width: 100%;
                 }
-    
+
                 /* Styling for the link button */
                 button[type=button] {
                     background-color: #4CAF50;
@@ -129,22 +329,22 @@ def main_page():
                     cursor: pointer;
                     width: 100%;
                 }
-    
+
                 /* Styling for the login button */
                 .loginbtn {
                     background-color: #4CAF50;
                 }
-    
+
                 /* Styling for the signup button */
                 .signupbtn {
                     background-color: #f44336;
                 }
-    
+
                 /* Styling for the cancel button */
                 .cancelbtn {
                     background-color: #ccc;
                 }
-    
+
                 /* Styling for the error message */
                 .error {
                     color: red;
@@ -154,7 +354,7 @@ def main_page():
             </style>
         </head>
         <body>
-            <form method="post" action="/login">
+            <form method="post" action="/token">
                 <div class="container">
                     <h1>Login</h1>
                     <hr>
@@ -166,7 +366,7 @@ def main_page():
                     <button type="submit" class="loginbtn">Login</button>
                 </div>
             </form>
-            
+
             <form method="post" action="/signup">
                 <div class="container">
                     <h1>Sign Up</h1>
@@ -183,44 +383,59 @@ def main_page():
     </html>
     """
 
+DATABASE_URL = "pie_chart.db"
 
-# Define a route to handle login request
-@app.post("/login")
-def login(username: str = Form(...), password: str = Form(...)):
-    conn = create_connection()
-    create_user_table(conn)
-    authenticated = authenticate_user(conn, username, password)
-    conn.close()
-    if authenticated:
-        return {"message": "Login successful"}
-    else:
-        return {"message": "Login failed"}
+def create_segments_table(conn):
+    try:
+        conn.execute('''CREATE TABLE IF NOT EXISTS segments
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                label TEXT NOT NULL,
+                value INTEGER NOT NULL);''')
+        print("Segments table created successfully")
+    except sqlite3.Error as e:
+        print(e)
+#create toy database
+def create_sample_data(conn):
+    sample_data = [
+        {"label": "Red", "value": 100},
+        {"label": "Blue", "value": 100},
+        {"label": "Yellow", "value": 100}
+    ]
 
-
-@app.post("/signup")
-def signup(username: str = Form(...), password: str = Form(...)):
-    conn = create_connection()
-    create_user_table(conn)
-    # Check if the username already exists
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username='{username}';")
-    user = cur.fetchone()
+    for data in sample_data:
+        cur.execute("SELECT COUNT(*) FROM segments WHERE label=?", (data["label"],))
+        count = cur.fetchone()[0]
 
-    if user:
+        if count == 0:
+            cur.execute("INSERT INTO segments (label, value) VALUES (?, ?)", (data["label"], data["value"]))
+        else:
+            cur.execute("UPDATE segments SET value=? WHERE label=?", (data["value"], data["label"]))
 
-        return {"message": "user already exists"}
-    else:
-        insert_user(conn, username, password)
-        return {"message": "succesful"}
+    conn.commit()
 
-    #if user:
-    #    conn.close()
-    # Redirect the user back to the sign-up page with an error message
-    #     return RedirectResponse("/signup?error=username_exists")
-    #else:
-    #
-    #    conn.close()
-    # Redirect the user back to the login page with a success message
+def get_segments(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT label, value FROM segments")
+    return cur.fetchall()
 
+conn = sqlite3.connect(DATABASE_URL)
+create_segments_table(conn)
+create_sample_data(conn)
 
-    #   return RedirectResponse("/login?message=signup_success")
+@app.get("/visualize", response_class=HTMLResponse)
+async def pie_chart(request: Request):
+    segments = get_segments(conn)
+    labels = [segment[0] for segment in segments]
+    values = [segment[1] for segment in segments]
+    print(labels)
+    print(values)
+
+    chart_data = {
+        "labels": labels,
+        "values": values,
+        "background_colors": ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+        "border_colors": ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"]
+    }
+
+    return templates.TemplateResponse("chart.html", {"request": request, "chart_data": json.dumps(chart_data)})
